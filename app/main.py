7b0784ae7e0fdcbb8673166174758d96c47e1307
Alex Nikitin <Nosify66@gmail.com>
2026-04-14 13:36:17 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -6,32 +6,40 @@ class Dictionary:
         self.capacity: int = capacity
         self.size: int = 0
         self.load_factor: float = 2 / 3
-        self.table: List[Optional[Tuple[Any, Any]]] = [None] * self.capacity
+        self.table: List[Optional[Tuple[Any, int, Any]]] = (
+            [None] * self.capacity
+        )
 
     def _hash(self, key: Any) -> int:
-        return hash(key) % self.capacity
+        return hash(key)
 
     def __setitem__(self, key: Any, value: Any) -> None:
         if self.size / self.capacity >= self.load_factor:
             self._resize()
 
-        index: int = self._hash(key)
+        full_hash: int = self._hash(key)
+        index: int = full_hash % self.capacity
+
         while self.table[index] is not None:
-            if self.table[index][0] == key:
-                self.table[index] = (key, value)
+            current_node = self.table[index]
+            if (current_node[1] == full_hash
+                    and current_node[0] == key):
+                self.table[index] = (key, full_hash, value)
                 return
             index = (index + 1) % self.capacity
 
-        self.table[index] = (key, value)
+        self.table[index] = (key, full_hash, value)
         self.size += 1
 
     def __getitem__(self, key: Any) -> Any:
-        index: int = self._hash(key)
+        full_hash: int = self._hash(key)
+        index: int = full_hash % self.capacity
         start_index: int = index
 
         while self.table[index] is not None:
-            if self.table[index][0] == key:
-                return self.table[index][1]
+            node = self.table[index]
+            if node[1] == full_hash and node[0] == key:  # type: ignore
+                return node[2]
             index = (index + 1) % self.capacity
             if index == start_index:
                 break
@@ -39,14 +47,24 @@ class Dictionary:
         raise KeyError(f"Key {key} not found")
 
     def _resize(self) -> None:
-        old_table: List[Optional[Tuple[Any, Any]]] = self.table
+        old_table: List[Optional[Tuple[Any, int, Any]]] = self.table
         self.capacity *= 2
         self.table = [None] * self.capacity
         self.size = 0
 
         for item in old_table:
             if item is not None:
-                self.__setitem__(item[0], item[1])
+                self._insert_during_resize(item[0], item[1], item[2])
+
+    def _insert_during_resize(
+        self, key: Any, full_hash: int, value: Any
+    ) -> None:
+        index: int = full_hash % self.capacity
+        while self.table[index] is not None:
+            index = (index + 1) % self.capacity
+
+        self.table[index] = (key, full_hash, value)
+        self.size += 1
 
     def __len__(self) -> int:
         return self.size
